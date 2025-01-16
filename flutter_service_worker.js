4312e4411a769de2b70a02a9ed67ed54d99348cf
name <brunooires14@gmail.com>
2025-01-16 14:47:10 +0000

    more changes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "7b9e5df29584a519813f3c472f650cfc",
+const RESOURCES = {"flutter_bootstrap.js": "67efb8a0ff2ab21ced3b371fb7bb9f2a",
 "version.json": "2b4d6a9d078137f6a1285c214191cc64",
-"index.html": "6690f3af0c4843cd349acc35eb06157b",
-"/": "6690f3af0c4843cd349acc35eb06157b",
-"main.dart.js": "0c2c97420d392b6f82e6ddb3fcc60cf6",
+"index.html": "4d152cbd871fda7adf38ab9d8a425939",
+"/": "4d152cbd871fda7adf38ab9d8a425939",
+"main.dart.js": "d8af3ba64f2b57f27fb31732639312f8",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
